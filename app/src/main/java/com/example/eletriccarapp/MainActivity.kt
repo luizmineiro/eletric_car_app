aabb8c05162013e644f47aef1f7d474d675a573e
luizmineiro <jluizmineiro@gmail.com>
2024-05-11 09:18:03 -0300

    first commit

---
+++ app/src/main/java/com/example/eletriccarapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.eletriccarapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
