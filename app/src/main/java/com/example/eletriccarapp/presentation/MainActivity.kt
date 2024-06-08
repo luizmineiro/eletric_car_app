86dd445f7cb9ef10bc008b53c4d52188e4a7ce90
luizmineiro <jluizmineiro@gmail.com>
2024-06-08 10:35:28 -0300

    trabalhando com listas no android

--- app/src/main/java/com/example/eletriccarapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/eletriccarapp/presentation/MainActivity.kt
@@ -3,22 +3,35 @@ package com.example.eletriccarapp.presentation
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ListView
 import android.widget.TextView
 import com.example.eletriccarapp.R
 
 class MainActivity : AppCompatActivity() {
     lateinit var btnCalcular : Button;
+    lateinit var lista : ListView;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setupViews();
         setupListeners();
+        setupLista();
     }
     fun setupViews(){
-          btnCalcular = findViewById(R.id.btn_calcular);
+        btnCalcular = findViewById(R.id.btn_calcular);
+        lista = findViewById(R.id.lv_infos);
+    }
+
+    fun setupLista(){
+        var dados = arrayOf(
+            "Tiramisu", "Snow Cone", "Red Velvet Cake", "Quince Tar", "Pistachio Ice Cream", "Oreo", "Nougat", "Marshmallow", "Lollipop", "KitKat",
+        )
+        var adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, dados)
+        lista.adapter = adapter
     }
 
     fun setupListeners(){
